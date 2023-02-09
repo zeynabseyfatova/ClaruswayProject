f6363603a94c4d82dadf15637f14cc70725355d1
zeynabseyfatova <zseyfatova@gmail.com>
2023-02-08 21:12:23 -0500

    Initial commit : I created Sdet2 project and Hello World class.

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+    System.out.println("Hello World");
+    System.out.print("Welcome to SDET Course :");
+    System.out.print("this line is starting with the previous line");
+    }
+}
